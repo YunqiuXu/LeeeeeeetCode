6421a2be69d692b2a1e4584e5168b97a9235effe
Yunqiu Xu <yunqiu_xu@163.com>
2017-01-04 08:35:37 +1100

    Array

---
+++ LC088_Merge_Sorted_Array.java
@@ -0,0 +1,25 @@
+public class Solution {
+    /** Yunqiu Xu*/
+    //A = [1,2,6,empty,empty], B = [4,5] --> [1,2,4,5,6]
+    //从后向前merge, 把比较大的元素放在靠后的位置
+    //所有的元素都放在nums1中
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int index = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[index--] = nums1[i--];
+            } 
+            else {
+                nums1[index--] = nums2[j--];
+            }
+        }
+        while (i >= 0) {
+            nums1[index--] = nums1[i--];
+        }
+        while (j >= 0) {
+            nums1[index--] = nums2[j--];
+        }
+    }
+}
